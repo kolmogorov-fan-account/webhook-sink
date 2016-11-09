247f68d4cb0c5122f13eb0e37d76e1516d247e10
Simon Génier <simon.genier@shopify.com>
2016-11-09 11:33:58 -0500

    Listen on all interfaces.

--- main.go
+++ main.go
@@ -8,16 +8,11 @@ import (
 func neverSayNo(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
-	address := os.Getenv("ADDRESS")
-	if address == "" {
-		address = "127.0.0.1"
-	}
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
 	http.HandleFunc("/", neverSayNo)
-	http.ListenAndServe(address+":"+port, nil)
+	http.ListenAndServe(":"+port, nil)
 }
